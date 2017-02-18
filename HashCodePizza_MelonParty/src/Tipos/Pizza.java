07f67e823ad0a586e5f14fdbfe11a585b7dbdfac
aloarte <aloarter@gmail.com>
2017-02-18 11:31:18 +0100

    * ADD: Pizza Class Getters & setters

--- HashCodePizza_MelonParty/src/Tipos/Pizza.java
+++ HashCodePizza_MelonParty/src/Tipos/Pizza.java
@@ -1,7 +1,9 @@
 package Tipos;
 
 /**
- * Created by p4r4d0x on 18/02/17.
+ * Created by aloarte on 18/02/17.
+ *
+ *
  */
 public class Pizza {
 
@@ -11,16 +13,64 @@ public class Pizza {
     }
 
 
-    Ingredient [][]     includedIngredients;
-    boolean[][]         slicesTaken;
+    public Ingredient[][] getIncludedIngredients() {
+        return includedIngredients;
+    }
+
+    public void setIncludedIngredients(Ingredient[][] includedIngredients) {
+        this.includedIngredients = includedIngredients;
+    }
+
+    public boolean[][] getSlicesTaken() {
+        return slicesTaken;
+    }
+
+    public void setSlicesTaken(boolean[][] slicesTaken) {
+        this.slicesTaken = slicesTaken;
+    }
+
+    public int getPizzaRows() {
+        return pizzaRows;
+    }
+
+    public void setPizzaRows(int pizzaRows) {
+        this.pizzaRows = pizzaRows;
+    }
+
+    public int getPizzaColumns() {
+        return pizzaColumns;
+    }
+
+    public void setPizzaColumns(int pizzaColumns) {
+        this.pizzaColumns = pizzaColumns;
+    }
+
+    public int getCurrentTomatoes() {
+        return currentTomatoes;
+    }
+
+    public void setCurrentTomatoes(int currentTomatoes) {
+        this.currentTomatoes = currentTomatoes;
+    }
+
+    public int getCurrentMushrooms() {
+        return currentMushrooms;
+    }
+
+    public void setCurrentMushrooms(int currentMushrooms) {
+        this.currentMushrooms = currentMushrooms;
+    }
+
+    private Ingredient [][]     includedIngredients;
+    private boolean[][]         slicesTaken;
 
     /* Pizza size */
-    int                 pizzaRows;
-    int                 pizzaColumns;
+    private int                 pizzaRows;
+    private int                 pizzaColumns;
 
     /* Num ingredients */
-    int                 currentTomatoes;
-    int                 currentMushrooms;
+    private int                 currentTomatoes;
+    private int                 currentMushrooms;
 
 
     public Pizza(int tomatoes ,int mushrooms, int row,int column, Ingredient[][] ingredients){
