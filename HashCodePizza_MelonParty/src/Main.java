46a9dba74321cfbc4621571dc79621e2350da40f
admachin <adol.adojma@gmail.com>
2017-02-18 13:54:35 +0100

    Add function test in Main

--- HashCodePizza_MelonParty/src/Main.java
+++ HashCodePizza_MelonParty/src/Main.java
@@ -1,5 +1,8 @@
 import Parser.ParserIn;
 import Tipos.Pizza;
+import Tipos.Position;
+import Tipos.Slice;
+import Utils.PizzaUtils;
 
 public class Main {
 
@@ -11,12 +14,15 @@ public class Main {
 		ParserIn parser = new ParserIn();
 		Pizza pizza = parser.parsePizzaFile("test.in");
 		System.out.println("Pizza parsed!");
-	
-	
-	
-
-
-
+		System.out.println(pizza);
+		test(pizza);
 	}
 
+	public static void test(Pizza pizza) {
+		Slice slice = new Slice(new Position(0, 0), new Position(1, 2), 3, 0);
+		if(PizzaUtils.isSliceValid(slice, slice.getSize(), 2))
+			System.out.println("Es válido");
+		else
+			System.out.println("No es válido");
+	}
 }
