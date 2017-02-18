6792b87037b15e5b877e29bd9daa00e9086797f1
Caparrini <Caparrini@users.noreply.github.com>
2017-02-18 14:55:21 +0100

    MODIFIED: Comments

--- HashCodePizza_MelonParty/src/Parser/ProblemEntry.java
+++ HashCodePizza_MelonParty/src/Parser/ProblemEntry.java
@@ -6,8 +6,13 @@ import Tipos.Pizza;
  * Created by Capa on 18/2/17.
  */
 public class ProblemEntry {
-
+    /**
+     * Maximum number of slice size
+     */
     public static int H;
+    /**
+     * Minimum number of T and M in a slice
+     */
     public static int L;
     public Pizza pizza;
 
