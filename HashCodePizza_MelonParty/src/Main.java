6e51e49c651bb3217bb6f5983aaa6a2f8e109744
aloarte <aloarter@gmail.com>
2017-02-19 18:44:14 +0100

    * ADD: Add firstindepth and firstinamplitude methods

--- HashCodePizza_MelonParty/src/Main.java
+++ HashCodePizza_MelonParty/src/Main.java
@@ -15,13 +15,13 @@ public class Main {
     public static void main(String[] args) {
 
         ProblemEntry data = ParserIn.parseEntryFile("small.in");
-        StatusNode solution = calculePizzaSlices(data);
+        StatusNode solution = calculePizzaSlicesFIRSTINDEPTH(data);
         ParserOut.generateOutputFile("small.out", solution);
         System.out.println("Solution finded!");
 
     }
 
-    public static StatusNode calculePizzaSlices(ProblemEntry data){
+    public static StatusNode calculePizzaSlicesFIRSTINDEPTH(ProblemEntry data){
         /**
          * Take pizza from data
          */
@@ -77,6 +77,70 @@ public class Main {
                      * Call for new nodes by expanding the first one
                      */
                     nodeList.addAll(0,Slicer.generateSlicers(nodeAux));
+                }
+            }
+        }
+
+        return null;
+
+    }
+
+    public static StatusNode calculePizzaSlicesFIRSTINAMPLITUDE(ProblemEntry data){
+        /**
+         * Take pizza from data
+         */
+        Pizza pizza = data.getPizza();
+
+
+        /**
+         * Create an arraylist with all the nodes
+         */
+        ArrayList<StatusNode> nodeList = new ArrayList<>();
+        nodeList.add(new StatusNode(0,pizza,new ArrayList<Slice>()));
+
+        Iterator<StatusNode> itr;
+        StatusNode nodeAux=null;
+
+        while(!nodeList.isEmpty()){
+            itr = nodeList.iterator();
+
+            System.out.println("Nodes at the list " + nodeList.size() + System.lineSeparator());
+
+            if(itr.hasNext()){
+                /**
+                 * Extract the first node of the list
+                 */
+                nodeAux=itr.next();
+                /**
+                 * Check if this node is solution
+                 */
+                if(nodeAux.isNodeASolution()){
+                    /*
+                    * If the solution is found, return;
+                    * */
+                    return nodeAux;
+
+                }
+                /**
+                 * Check if this node can not generate a new valid solution
+                 */
+                else if(nodeAux.needIngredients()){
+                    /**
+                     * Remove without expanding
+                     */
+                    nodeList.remove(nodeAux);
+
+                }
+                else{
+                    /**
+                     * Remove node from list
+                     */
+                    nodeList.remove(nodeAux);
+
+                    /**
+                     * Call for new nodes by expanding the first one
+                     */
+                    nodeList.addAll(Slicer.generateSlicers(nodeAux));
                 }
             }
         }
